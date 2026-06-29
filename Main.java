33c2f4bec9f519485d8ec6898967108e912db2be
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2026-06-29 04:01:25 +0000

    Initial commit: Estructura básica

---
+++ Main.java
@@ -0,0 +1,1 @@
+public class Main { public static void main(String[] args) { Saludo saludo = new Saludo(); System.out.println(saludo.generarSaludo("Mundo", "es")); } }
---
+++ Saludo.jav
@@ -0,0 +1,1 @@
+public class Saludo { public String generarSaludo(String nombre, String idioma) { return "Hola, " + nombre; } }
