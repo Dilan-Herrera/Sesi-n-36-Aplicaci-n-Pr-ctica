cb73f3893e2a58a876eda974f3f041d436d7e3dd
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2026-06-29 04:04:09 +0000

    Añadida funcionalidad de despedida

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
-public class Main { public static void main(String[] args) { Saludo saludo = new Saludo(); System.out.println(saludo.generarSaludo("Mundo", "es")); } }
+public class Main {
+    public static void main(String[] args) {
+        Saludo saludo = new Saludo();
+        System.out.println(saludo.generarSaludo("Mundo", "es"));
+
+        // Añadido para Ejercicio 1
+        Despedida despedida = new Despedida();
+        System.out.println(despedida.generarDespedida("Mundo"));
+    }
+}
